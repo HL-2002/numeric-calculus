a9da0d8f72bcb8228690f937a5b583955165fcda
HL-2002 <henryslang8231682@gmail.com>
2024-02-07 11:41:18 -0400

    added docstring to function and refactored number h->0 in the approximation

--- project-2/Taylor_interpolation.py
+++ project-2/Taylor_interpolation.py
@@ -1,8 +1,18 @@
 import numpy as np 
 
-def Taylor_interpolation(f, a, x):
+def Taylor_interpolation(f:callable, a:float, x:float) -> float:
+    """Does first degree interpolation of a function.
+
+    Args:
+        f (function): function to approximate
+        a (float): point of expansion
+        x (float): interpolation point
+
+    Returns:
+        (float): interpolation of f around a at x.
+    """
     # Calculate the first derivative of f(x)
-    f_prime = (f(a + 0.00001) - f(a - 0.00001)) / (2 * 0.00001)
+    f_prime = (f(a + 1e-5) - f(a - 1e-5)) / (2 * 1e-5)
     
     # Calculate the first-order Taylor approximation
     approximation = f(a) + f_prime * (x - a)
@@ -16,13 +26,16 @@ def Taylor_interpolation(f, a, x):
 def f(x):
     return np.sin(x)
 
-# Set the point of expansion a = pi/4
-a = np.pi/4
+if __name__ == "__main__":
+    # Set the point of expansion a = pi/4
+    a = np.pi/4
 
-# Set the value of x = pi/2
-x = np.pi/2
+    # Set the value of x = pi/2
+    x = np.pi/2
 
-# Calculate the interpolated value using Taylor_interpolation
-interpolated_value = Taylor_interpolation(f, a, x)
+    # Calculate the interpolated value using Taylor_interpolation
+    interpolated_value = Taylor_interpolation(f, a, x)
 
-print(f"El valor interpolado de f es -> {x:.4f} \nUsando la aproximación de Taylor de primer orden es -> {interpolated_value:.4f}")
+    print(f"El valor interpolado de f es -> {a:.4f}",
+          f"Usando la aproximación de Taylor de primer orden es -> {interpolated_value:.4f}",
+          sep="\n")
